84afa197c19bf9aaa273ae053fcaf92119349ff0
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-07 19:17:15 +0530

    Added : Program to perform bubble sort

--- src/com/bridgelabz/binarysearch/BinarySearch.java
+++ src/com/bridgelabz/binarysearch/BinarySearch.java
@@ -13,26 +13,38 @@ public class BinarySearch
 		System.out.println("Enter the word to find");
 		String value  = scanner.next().toLowerCase();
 		scanner.close();
+		
+		//converting string to array
 		String[] words = str.toLowerCase().split(" ");
+		
+		//sorting the elements in arrays
 		Arrays.sort(words);
+		
+		//assigning minimum and maximum value
 		int min = 0;
 		int max = words.length;
+	
+		// computation for binary search
 		while(min <= max  )
 		{
 			int mid = (min+max) / 2 ;
+			//mid value equals to value
 			if(value.compareTo(words[mid]) == 0)
 			{
 				System.out.println(value+" Found ");
 				break;
 			}
+			//value is less than mid value 
 			else if (value.compareTo(words[mid] )< 0)
 				{
 					max = mid-1;
 				}
+			//value is greater than mid value 
 			else 
 			{
 				min = mid+1;
 			}
+			//if element not found
 			if(min > max)
 			{
 				System.out.println(value+" not found");
---
+++ src/com/bridgelabz/bubblesort/BubbleSort.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.bubblesort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BubbleSort 
+{
+	public static void main(String[] args) 
+	{		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the string");
+		String str = scanner.nextLine();
+		scanner.close();
+		
+		//converting string to array
+		String[] arrayToSort = str.toLowerCase().split(" ");
+		
+		//computation for bubble sort
+		for (int i = 0; i < arrayToSort.length; i++) 
+		{
+			for (int j = i; j < arrayToSort.length; j++) 
+			{
+				//swap value if value > next value
+				if(arrayToSort[i].compareTo(arrayToSort[j]) > 0 )
+				{
+					String temp = arrayToSort[i];
+					arrayToSort[i] = arrayToSort[j];
+					arrayToSort[j] = temp;
+				}
+			}
+		}
+		System.out.println("After sorting : "+Arrays.toString(arrayToSort));
+	}
+}
